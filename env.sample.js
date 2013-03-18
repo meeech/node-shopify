dcf378b34bb67a46c5f3ba982b81ef6611ba1f01
Mitchell Amihod <mitchell.amihod@gmail.com>
2013-03-18 11:21:32 -0400

    add username / password for testing

--- env.sample.js
+++ env.sample.js
@@ -1,5 +1,9 @@
 //Rename this file to env.js
-//Sets up process.env values for running tests and example so we don't have to 
+//Sets up process.env values for running tests and example so we don't have to
 //commit to repo
 process.env['SHOPIFY_HOST'] = "SHOP-NAME.myshopify.com";
 process.env['SHOPIFY_TOKEN'] = "yourauthtokenhere";
+
+//Used for testing.
+process.env['SHOP_USERNAME'] = "SHOPIFY_HOST_USERNAME";
+process.env['SHOP_PASSWORD'] = "SHOPIFY_HOST_PASSWORD";
